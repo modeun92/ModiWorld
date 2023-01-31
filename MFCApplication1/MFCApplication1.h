1799e78daa70a3d6f05b43d5807a9d07a0e1a2df
Modeun Kang <43582540+modeun92@users.noreply.github.com>
2023-01-31 11:15:22 +0900

    I put the project in the repository
    This works fine.
    you can simply create the solution in the location of the repository.

---
+++ MFCApplication1/MFCApplication1.h
@@ -0,0 +1,40 @@
+﻿
+// MFCApplication1.h: MFCApplication1 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMFCApplication1App:
+// 이 클래스의 구현에 대해서는 MFCApplication1.cpp을(를) 참조하세요.
+//
+
+class CMFCApplication1App : public CWinAppEx
+{
+public:
+	CMFCApplication1App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication1App theApp;

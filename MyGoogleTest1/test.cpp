82d8786862876f31aa0b6c998f1d1052003829c0
Modeun Kang <43582540+modeun92@users.noreply.github.com>
2023-02-01 10:35:08 +0900

    UnitTestModify&Build
    I changed some of  the google test scripts and I built the solution with MSBuild.exe.
    It worked so well and I will try to add this in actions soon.

--- MyGoogleTest1/test.cpp
+++ MyGoogleTest1/test.cpp
@@ -1,6 +1,27 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, TestName1) {
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(2, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(4, 1);
+  EXPECT_EQ(1, 1);
+}
+TEST(TestCaseName, TestName2) {
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	ASSERT_EQ(4, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(1, 1);
+	EXPECT_EQ(4, 1);
 }
